6c65486d599b9a31e8c9d4717deacb3e952458a1
Harshil Shah <harshilhshah1995@gmail.com>
2016-03-17 12:00:14 -0400

    normal commit

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20160316204215) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "charges", force: :cascade do |t|
     t.float    "amount"
     t.boolean  "refunded"
